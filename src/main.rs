922df8121b0075582574ae2f93f2ea737fb4a12e
Zyansheep <zyansheep@gmail.com>
2020-06-29 17:13:14 -0400

    world and port value parsing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,27 @@
 extern crate clap;
 use clap::App;
 
+use std::path::Path;
+
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+	let yaml = load_yaml!("cli.yml");
+	let matches = App::from_yaml(yaml).get_matches();
+		
+	// Figure out where world file and config files are
+	let mut world_path = Path::new("world.wld");
+	if let Some(world) = matches.value_of("world") {
+		world_path = Path::new(world);
+	} else if !world_path.is_file() {
+		panic!("World file is not file: {}", world_path.display());
+	}
+	//let world = World::new();
+	//world.load(world_path).unwrap(); //This should call path.exists() and return error accordingly
+
+	let mut port: u16 = 7777;
+	if let Some(port_str) = matches.value_of("port") {
+		port = port_str.parse().expect("Error, port value not correct");
+	}
+
+	//let server = Server::new();
+	//server.start(port, world, etc...)
 }
