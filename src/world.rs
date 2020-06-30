4cba3da3d00f908bff734326119e752e4a133831
cphos <stonefox317@gmail.com>
2020-06-30 11:17:17 -0400

    World file format header reading

--- src/world.rs
+++ src/world.rs
@@ -1,13 +1,62 @@
-pub struct World {
+extern crate byteorder;
 
+use std::fs::File;
+use std::io;
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+
+#[derive(Default)]
+pub struct World {
+    version: i32,
+    revision: u32,
+    is_favorite: bool,
 }
 
 impl World {
-    pub fn open(path: &str) -> World {
-        World {}
+    pub fn read_from_file(path: &str) -> Result<World, io::Error> {
+        let mut file = File::open(path)?;
+        let wld = World::read(&mut file)?;
+
+        Ok(wld)
     }
 
-    pub fn save(&self, path: &str) {
+    pub fn write_to_file(&self, path: &str) -> Result<(), io::Error> {
+        let mut file = File::create(path)?;
+        self.write(&mut file)?;
 
+        Ok(())
+    }
+
+    pub fn read(rdr: &mut dyn io::Read) -> Result<World, io::Error> {
+        let mut wld = World::default();
+
+        wld.read_file_format_header(rdr)?;
+
+        Ok(wld)
+    }
+
+    fn read_file_format_header(&mut self, rdr: &mut dyn io::Read) -> Result<(), io::Error> {
+        self.version = rdr.read_i32::<LittleEndian>()?;
+        rdr.read_u64::<LittleEndian>()?; // Magic + filetype (we are assuming that it is a world file.)
+        self.revision = rdr.read_u32::<LittleEndian>()?;
+        self.is_favorite = rdr.read_u64::<LittleEndian>()? != 0;
+
+        Ok(())
+    }
+
+    pub fn write(&self, wtr: &mut dyn std::io::Write) -> Result<(), io::Error> {
+        self.write_file_format_header(wtr)?;
+
+        Ok(())
+    }
+
+    fn write_file_format_header(&self, wtr: &mut dyn io::Write) -> Result<(), io::Error> {
+        wtr.write_i32::<LittleEndian>(self.version)?;
+        wtr.write(b"relogic")?;
+        wtr.write_u8(2)?;
+        wtr.write_u32::<LittleEndian>(self.revision)?;
+        wtr.write_u64::<LittleEndian>(self.is_favorite as u64)?;
+
+        Ok(())
     }
 }
