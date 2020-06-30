5599256f01c6d7749cc95a59da8fbbd9dc91e6df
cphos <stonefox317@gmail.com>
2020-06-30 12:04:08 -0400

    Chunk offsets

--- src/world.rs
+++ src/world.rs
@@ -6,7 +6,6 @@ use std::io;
 
 #[derive(Default)]
 pub struct World {
-    version: i32,
     revision: u32,
     is_favorite: bool,
 }
@@ -35,26 +34,43 @@ impl World {
     }
 
     fn read_file_format_header(&mut self, rdr: &mut impl io::Read) -> Result<(), io::Error> {
-        self.version = rdr.read_i32::<LittleEndian>()?;
+        rdr.read_i32::<LittleEndian>()?; // Version (we are assuming that it is 230.)
+
+        // File metadata.
         rdr.read_u64::<LittleEndian>()?; // Magic + filetype (we are assuming that it is a world file.)
         self.revision = rdr.read_u32::<LittleEndian>()?;
         self.is_favorite = rdr.read_u64::<LittleEndian>()? != 0;
 
+        // Chunk offsets.
+        let chunk_count = rdr.read_i16::<LittleEndian>()?;
+        let mut chunk_offsets = vec![0; chunk_count as usize];
+        for i in 0..chunk_count {
+            chunk_offsets[i as usize] = rdr.read_i32::<LittleEndian>()?;
+        }
+
         Ok(())
     }
 
-    pub fn write(&self, wtr: &mut impl std::io::Write) -> Result<(), io::Error> {
+    pub fn write(&self, wtr: &mut (impl io::Write + io::Seek)) -> Result<(), io::Error> {
         self.write_file_format_header(wtr)?;
 
         Ok(())
     }
 
-    fn write_file_format_header(&self, wtr: &mut impl io::Write) -> Result<(), io::Error> {
-        wtr.write_i32::<LittleEndian>(self.version)?;
+    fn write_file_format_header(&self, wtr: &mut (impl io::Write + io::Seek)) -> Result<(), io::Error> {
+        wtr.write_i32::<LittleEndian>(230)?; // Version.
+
+        // File metadata.
         wtr.write(b"relogic")?; // Magic.
         wtr.write_u8(2)?; // Filetype.
         wtr.write_u32::<LittleEndian>(self.revision)?;
         wtr.write_u64::<LittleEndian>(self.is_favorite as u64)?;
+
+        // Chunk offsets.
+        wtr.write_i16::<LittleEndian>(11)?; // Chunk count.
+        for _ in 0..11 {
+            wtr.write_i32::<LittleEndian>(0)?; // Placeholders for after writing all chunks.
+        }
 
         Ok(())
     }
